f6993991bf80f64f87dd8ebf93008aa5738f130c
marianybegasilva-blip <mariany.bega.silva@escola.pr.gov.br>
2026-09-14 11:54:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+ const botao = document.querySelector("button");
+
+    botes.forEach(function (botao) {
+           let curtiu = false; 
+    botao.addEventListener("click", botaoClicado);
+    function botaoClicado() {
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+        if (curtiu === false) {
+        texto.textContent++;
+        curtiu = true;
+        } else{
+        text.textContent--;
+        curtiu = false;
+        }
+    }
+  })
 
